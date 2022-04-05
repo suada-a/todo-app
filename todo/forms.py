66f43feecfc95f9bfb7ec6701e2fe6644ad7a814
suada-a <suadaabdurahim@yahoo.com>
2022-04-04 23:43:58 -0400

    Complete task editing functionality

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, validators
+from wtforms import StringField, SubmitField, IntegerField, validators
 from wtforms.validators import DataRequired
 
 class AddTaskForm(FlaskForm):
+    description = StringField('description', [validators.Length(min=4, max=100), DataRequired()])
+    submit = SubmitField(label='Submit')
+
+class EditTaskForm(FlaskForm):
+    task_id = IntegerField()
     description = StringField('description', [validators.Length(min=4, max=100), DataRequired()])
     submit = SubmitField(label='Submit')
--- todo/routes.py
+++ todo/routes.py
@@ -1,21 +1,47 @@
 from todo import app
 from flask import render_template, redirect, url_for
 from todo.models import Task
-from todo.forms import AddTaskForm
+from todo.forms import *
 from todo import db
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 @app.route('/home')
 def home_page():
     add_task_form = AddTaskForm()
+    edit_task_form = EditTaskForm()
+    
+    tasks = Task.query.all()
+    return render_template('index.html', add_task_form=add_task_form, 
+    edit_task_form=edit_task_form, tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    add_task_form = AddTaskForm()
+    edit_task_form = EditTaskForm()
+
     if add_task_form.validate_on_submit():
         new_task = Task(description=add_task_form.description.data)
         db.session.add(new_task)
         db.session.commit()
         return redirect(url_for('home_page'))
+    
+    return render_template('index.html', add_task_form=add_task_form, edit_task_form=edit_task_form)
 
-    tasks = Task.query.all()
-    return render_template('index.html', add_task_form=add_task_form, tasks=tasks)
+@app.route('/edit', methods=['POST'])
+def edit():
+    add_task_form = AddTaskForm()
+    edit_task_form = EditTaskForm()
+
+    if edit_task_form.validate_on_submit():
+        new_description = edit_task_form.description.data
+        current_task_id = edit_task_form.task_id.data
+        current_task = Task.query.get(current_task_id)
+        current_task.description = new_description
+        db.session.commit()
+
+        return redirect(url_for('home_page')) 
+    
+    return render_template('index.html', add_task_form=add_task_form, edit_task_form=edit_task_form)
 
 @app.route('/register')
 def register_page():
